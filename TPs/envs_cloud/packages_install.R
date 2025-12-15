c1c2f37b40d4b2508370ec5415aaae2339d93535
Yoann Pradat <yoann.pradat@gustaveroussy.fr>
2025-12-15 11:10:29 +0100

    add missing library + improve script for cloud install

--- TPs/envs_cloud/packages_install.R
+++ TPs/envs_cloud/packages_install.R
@@ -1,21 +1,37 @@
+# --- 1. Global Setup ---
+# Set the CRAN mirror globally so you don't have to type it every time.
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+
+# --- 2. Setup BiocManager ---
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 
-# packages installable via BiocManager::install
-packages_biocmanager_required <- c("maftools", "BSgenome", "BSgenome.Hsapiens.UCSC.hg19", "MutationalPatterns",
-                                   "GenomicDataCommons")
-for (package_biocmanager_required in packages_biocmanager_required){
-  if (!require(package_biocmanager_required, quietly = TRUE))
-    BiocManager::install(package_biocmanager_required)
+# --- 3. Define Package Lists ---
+bioc_pkgs <- c("maftools", "BSgenome", "BSgenome.Hsapiens.UCSC.hg19", "MutationalPatterns")
+cran_pkgs <- c("rmarkdown", "RColorBrewer", "glmnet", "dplyr", "tidyr",
+               "yarrr", "knitr", "randomForest", "ICAMS")
+
+# --- 4. Install Missing Packages in Bulk ---
+
+# Check/Install Bioconductor
+missing_bioc <- bioc_pkgs[!(bioc_pkgs %in% installed.packages()[,"Package"])]
+if(length(missing_bioc) > 0) {
+  message("Installing missing Bioconductor packages...")
+  BiocManager::install(missing_bioc, update = FALSE, ask = FALSE)
 }
 
-# packages installable via install.packages
-packages_required <- c("rmarkdown", "RColorBrewer", "glmnet", "dplyr", "tidyr", "yarrr", "knitr", "randomForest",
-                       "ICAMS")
-
-for (package_required in packages_required){
-  if (!require(package_required, quietly = TRUE))
-    install.packages(package_required)
+# Check/Install CRAN
+missing_cran <- cran_pkgs[!(cran_pkgs %in% installed.packages()[,"Package"])]
+if(length(missing_cran) > 0) {
+  message("Installing missing CRAN packages...")
+  # No need to specify repo here because we set it in 'options' above
+  install.packages(missing_cran)
 }
 
-
+# --- 5. Load All Packages ---
+all_pkgs <- c(bioc_pkgs, cran_pkgs)
+invisible(lapply(all_pkgs, function(pkg) {
+  if (!require(pkg, character.only = TRUE, quietly = TRUE)) {
+    warning(paste("Package", pkg, "failed to load!"))
+  }
+}))
